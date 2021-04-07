fe3c4f8f98723afcf168f99e2b2bbeb8efa57ba0
piyushsawhney <!(^Git!(^94>
2021-04-07 19:07:35 +0530

    cu-#2nbb4f Common selenium workflows

---
+++ common_scenarios.py
@@ -0,0 +1,39 @@
+import json
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+import driver
+import element_ids as IDs
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class LoginPage:
+    @staticmethod
+    def GoToURL(url):
+        driver.Instance.get(url)
+
+    @staticmethod
+    def Login():
+        with open('config/credentials.json', "r") as json_file:
+            config = json.load(json_file)
+        username = config['username']
+        password = config['password']
+        driver.Instance.find_element_by_id(IDs.login_elements['username']).send_keys(username)
+        driver.Instance.find_element_by_id(IDs.login_elements['password']).send_keys(password)
+        driver.Instance.find_element_by_id(IDs.login_elements['login']).click()
+
+    @staticmethod
+    def navigate_to_accounts():
+        long_waits = WebDriverWait(driver.Instance, 30, poll_frequency=5)
+        short_waits = WebDriverWait(driver.Instance, 10, poll_frequency=1)
+        element = short_waits.until(EC.element_to_be_clickable((By.ID, IDs.navigation_elements['accounts'])))
+        element.click()
+        element = long_waits.until(EC.element_to_be_clickable((By.ID, IDs.navigation_elements['deposit_accounts'])))
+        element.click()
+        long_waits.until(EC.element_to_be_clickable((By.ID, IDs.navigation_elements['fetch'])))
+
+    @staticmethod
+    def logout():
+        driver.Instance.find_element_by_id(IDs.login_elements['logout_1']).click()
+        driver.Instance.find_element_by_id(IDs.login_elements['logout_2']).click()

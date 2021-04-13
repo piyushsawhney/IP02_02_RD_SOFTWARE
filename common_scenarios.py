c35b9087125467b4befe087e6a0af93fbc010f3b
piyushsawhney <!(^Git!(^94>
2021-04-13 21:34:05 +0530

    cu-#2nbb4f Adding navigation to reports

--- common_scenarios.py
+++ common_scenarios.py
@@ -34,6 +34,16 @@ class LoginPage:
         long_waits.until(EC.element_to_be_clickable((By.ID, IDs.navigation_elements['fetch'])))
 
     @staticmethod
+    def navigate_to_reports():
+        long_waits = WebDriverWait(driver.Instance, 30, poll_frequency=5)
+        short_waits = WebDriverWait(driver.Instance, 10, poll_frequency=1)
+        element = short_waits.until(EC.element_to_be_clickable((By.ID, IDs.navigation_elements['accounts'])))
+        element.click()
+        element = long_waits.until(EC.element_to_be_clickable((By.ID, IDs.navigation_elements['reports'])))
+        element.click()
+        long_waits.until(EC.visibility_of_element_located((By.ID, IDs.navigation_elements['agent_id'])))
+
+    @staticmethod
     def logout():
         driver.Instance.find_element_by_id(IDs.login_elements['logout_1']).click()
         driver.Instance.find_element_by_id(IDs.login_elements['logout_2']).click()

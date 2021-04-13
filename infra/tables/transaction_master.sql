284536cb08442bc8a9f8a856da2347d6e77a0306
piyushsawhney <!(^Git!(^94>
2021-04-13 21:36:42 +0530

    cu-#2nb9vz Removing extra columns

--- infra/tables/transaction_master.sql
+++ infra/tables/transaction_master.sql
@@ -2,21 +2,13 @@ do $$ declare
 schema_name_variable constant varchar := 'test';
 client_table_variable constant varchar := 'rd_account_transactions';
 begin
---DROP DOMAIN cheque_numbers;
---create DOMAIN cheque_numbers AS INTEGER CHECK (
---   VALUE > 0 AND VALUE <= 999999
---);
 execute format(
 'CREATE TABLE IF NOT EXISTS %I.%I(
    rd_date date CONSTRAINT check_date CHECK((EXTRACT(DAY from rd_date) = 1) or EXTRACT(DAY from rd_date) = 16) ,
-   receive_date date,
    account_no varchar(12),
-   investor_name varchar,
    is_cash boolean,
    no_of_installments smallint,
-   deposit_amount numeric,
-   cheque_number cheque_numbers,
-   bank_name varchar,
+   cheque_number varchar(6),
    bank_account_no varchar,
    loan_amount numeric,
    loan_interest numeric,
@@ -24,7 +16,7 @@ execute format(
    rebate numeric,
    default_fee numeric,
    schedule_date date,
-   schedule_number varchar(10),
+   schedule_number varchar(12),
    CONSTRAINT cheque_schedule CHECK ((is_cash = False and cheque_number IS NOT NULL AND bank_account_no IS NOT NULL) OR is_cash = True),
    PRIMARY KEY (rd_date, account_no),
    CONSTRAINT fk_account_no

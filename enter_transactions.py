418252e4c5edc2b37b3427023c3360bc64e594e9
piyushsawhney <!(^Git!(^94>
2021-06-11 20:37:12 +0530

    cu-#3qb43k

---
+++ enter_transactions.py
@@ -0,0 +1,32 @@
+import json
+import datetime
+
+from openpyxl import load_workbook
+
+from database import execute_insert_query
+
+with open('config/db_config.json', "r") as json_file:
+    config_json = json.load(json_file)
+    file_name = config_json['file_name']
+
+
+def enter_into_transaction_table(insert_dict):
+    execute_insert_query("transaction", insert_dict)
+
+
+if __name__ == '__main__':
+    wb = load_workbook(file_name)
+    ws = wb['Sheet1']
+    today_date = datetime.date.today()
+    if today_date.day <= 15:
+        date = datetime.date(today_date.year, today_date.month, 1)
+    else:
+        date = datetime.date(today_date.year, today_date.month, 1)
+
+    for row in ws.iter_rows(min_row=2):
+        insert_dict = {"account_no": row[0].value, "rd_date": str(date), "schedule_group": (int(row[4].value)),
+                       "is_cash": bool(row[5].value), "no_of_installments": int(row[2].value)}
+        if row[6].value:
+            insert_dict['cheque_number'] = row[6].value
+            insert_dict['is_cash']: False
+        enter_into_transaction_table(insert_dict)
